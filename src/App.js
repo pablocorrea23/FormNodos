582550675a12f9a9f8aba30436fa39b3912241ca
pablocorrea23 <pablocorrea34@gmail.com>
2022-08-10 10:57:05 -0300

    cambio2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { Formulario } from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
+      <Formulario/>
+
       </header>
     </div>
   );
